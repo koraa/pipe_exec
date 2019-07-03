2e102fb71ae4746bb57b95418393714887b116f3
Karolin Varner <karo@cupdev.net>
2019-07-03 22:34:59 +0200

    get rid of unneded close()

--- pexec.cc
+++ pexec.cc
@@ -18,10 +18,6 @@ T cksys(const char *msg, T r) {
   _exit(1);
 }
 
-void close(int fd) {
-  cksys("close()", ::close(fd));
-}
-
 size_t read(int fd, void *buf, size_t count) {
   if (count == 0) return 0;
   while (true) {
