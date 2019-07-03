3a241b89f90fb118689bd51dcb1ee5f443226c21
Karolin Varner <karo@cupdev.net>
2019-07-03 21:54:20 +0200

    Simplify code
    Reduce C++ dependency & remove unnecessary code

--- pexec.cc
+++ pexec.cc
@@ -1,96 +1,51 @@
+extern "C" {
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <linux/memfd.h>
+}
 
-#include <cstring>
-
-#include <string>
-#include <sstream>
 #include <vector>
-#include <iostream>
 
-#define UNUSED __attribute__ ((unused))
+namespace pexec {
 
-/// Represents an error condition with a dynamic error
-/// message
-struct err : std::exception {
-  std::string _what;
+template<typename T=int>
+T cksys(const char *msg, T r) {
+  if (r >= 0) return r;
+  fprintf(stderr, "Fatal Error in %s: %s", msg, strerror(r));
+  _exit(1);
+}
 
-  err(const std::string &msg, int errnum) {
-    std::stringstream ss;
-    ss << msg << " ERROR " << errnum << ": " << strerror(errnum);
-    _what = ss.str();
+void close(int fd) {
+  cksys("close()", ::close(fd));
+}
+
+size_t read(int fd, void *buf, size_t count) {
+  if (count == 0) return 0;
+  while (true) {
+    auto r = ::read(fd, buf, count);
+    if (r < 0 && errno == EINTR) continue;
+    return cksys("read()", r);
   }
+}
 
-  err(const std::string &msg) : _what(msg) {}
-  err(int errnum) : err("", errnum) {}
-
-  const char* what() const noexcept {
-    return _what.c_str();
+size_t write(int fd, void *buf, size_t count) {
+  if (count == 0) return 0;
+  while (true) {
+    auto r = ::write(fd, buf, count);
+    if (r < 0 && errno == EINTR) continue;
+    return cksys("write()", r);
   }
-};
-
-/// Strict file descriptor; auto closes the fd when it goes
-/// out of scope
-struct xfd {
-  int fd;
-
-  xfd(int fd) : fd(fd) {}
-  ~xfd() {
-    close(fd);
-  }
-};
-
-template<typename V=int>
-V ckerrno(V r, const std::string &msg) {
-  if (r < 0) throw err(msg, errno);
-  return r;
 }
 
-/// The x... family of calls correspond to their
-/// syscall/glibc api, but all of them are checked; they
-/// will throw exceptions when an error ocurrs
-void xclose(int fd) {
-  ckerrno(close(fd), "close()");
-}
-
-size_t xread(int fd, void *buf, size_t count) {
-  auto r = read(fd, buf, count);
-  if (r == -1 && errno == EINTR)
-    return xread(fd, buf, count);
-  else
-    return ckerrno(r, "read()");
-}
-
-size_t xwrite(int fd, void *buf, size_t count) {
-  auto r = write(fd, buf, count);
-  if (r == -1 && errno == EINTR)
-    return xwrite(fd, buf, count);
-  else
-    return ckerrno(r, "write()");
-}
-
-void xfexecve(int fd, char *const argv[], char *const envp[]) {
-  ckerrno(fexecve(fd, argv, envp), "fexecve()");
-  throw err("fexecve() ERROR: Should have terminated cexec");
-}
-
-xfd xmemfd_create(const char *name, unsigned int flags=0) {
-  int r = syscall(SYS_memfd_create, name, flags);
-  ckerrno(r, "memfd_create()");
-  return xfd(r);
-}
-
-/// Like xwrite but makes sure really all data is written or
-/// an error is thrown; xwrite may succeed only having
+/// Like write but makes sure really all data is written or
+/// an error is thrown; write may succeed only having
 /// written some of the data.
-void xwrite_all(int fd, void *buf, size_t count) {
-  size_t written = 0, iter=0;
-  for (; written < count && iter < 2000; iter++)
-    written += xwrite(fd, (char*)buf+written, count - written);
-
-  if (iter > 2000)
-    throw("write() ERROR Giving up trying zero bytes lots of times");
+void write_all(int fd, void *buf, size_t count) {
+  for (size_t written = 0; written < count; )
+    written += write(fd, (char*)buf+written, count - written);
 }
 
 /// Stream the contents of one fd into another
@@ -125,48 +80,25 @@ void transfer(int fdin, int fdout) {
   size_t buf_len = 1;
 
   while (1) {
-    buf_len = xread(fdin, &buf[0], buf.size());
+    buf_len = read(fdin, &buf[0], buf.size());
     if (buf_len == 0) break; // EOF
-    xwrite_all(fdout, &buf[0], buf_len);
+    write_all(fdout, &buf[0], buf_len);
   }
 }
 
-template<typename F>
-bool drop_exceptions(F f) {
-  try {
-    f();
-    return true;
-  } catch (std::exception &e) {
-    return false;
-  }
+extern "C" int main(int, char *argv[]) {
+  // Try executing stdin in place; if this works, execution
+  // of this program will terminate, so we can assume that some
+  // error occurred if the program keeps going
+  ::fexecve(0, argv, __environ);
+
+  // OK; it's probably a file; copy into a anonymous, memory backed
+  // temp file, then it should work
+  const auto f = cksys("memfd_create()", ::syscall(SYS_memfd_create, "Virtual File", MFD_CLOEXEC));
+  transfer(0, f);
+  cksys("fexecve()", ::fexecve(f, argv, __environ));
+  fprintf(stderr, "Fatal Error in fexecve(): Should have terminated the process");
+  return 1;
 }
 
-template<typename F>
-bool print_exceptions(F f) {
-  try {
-    f();
-    return true;
-  } catch (std::exception &e) {
-    std::cerr << "[ERROR] " << e.what() << std::endl;
-    return false;
-  }
-}
-
-template<typename F>
-void fatal_exceptions(F f) {
-  if (!print_exceptions(f)) exit(1);
-}
-
-int main(int argc UNUSED, char *argv[]) {
-  fatal_exceptions([&]() {
-
-    drop_exceptions([&]() {
-      xfexecve(0, argv, __environ);
-    });
-
-    const xfd vfd = xmemfd_create("Virtual file", MFD_CLOEXEC);
-    transfer(0, vfd.fd);
-    xfexecve(vfd.fd, argv, __environ);
-
-  });
 }
